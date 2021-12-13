00372a1cfcfa78c32ed093919ccd5edcee07ed52
Mohamed Ali <52932544+muhamed3li@users.noreply.github.com>
2021-12-13 15:43:32 +0200

    first commit

---
+++ app/products.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class products extends Model
+{
+//    protected $fillable = [
+//        'products_name',
+//        'description',
+//        'section_id',
+//
+//    ];
+
+
+      protected $guarded = [];
+
+      public function section()
+    {
+      return $this->belongsTo('App\sections');
+    }
+}
